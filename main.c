fb97eda4da316251026afdd1f9d8504d7d6c516d
Marios Tsalkidis <marios.tsalkidis@gmail.com>
2017-07-05 15:59:31 +0300

    Fibo original

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <time.h>
 
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
+long timediff(clock_t t1, clock_t t2) {
+    long elapsed;
+    elapsed = ((double)t2 - t1) / CLOCKS_PER_SEC * 1000;
+    return elapsed;
 }
 
+long fibonacci(long i) {
+
+   if(i == 0) {
+      return 0;
+   }
+
+   if(i == 1) {
+      return 1;
+   }
+   return fibonacci(i-1) + fibonacci(i-2);
+}
+
+int  main() {
+
+   clock_t t1, t2;
+   long elapsed;
+   int diff;
+   long i;
+
+   for (i = 0; i < 100; i++) {
+      t1 = clock();
+      printf("%d",i);
+      printf(" : %d", fibonacci(i));
+      t2 = clock();
+      elapsed = timediff(t1, t2);
+      printf(" - took : %ld ms\t\n", elapsed);
+   }
+
+   printf("\n");
+   return 0;
+}
